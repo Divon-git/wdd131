4332b84eda6aaec7f3c6b031785e2b359110d8a9
Divon-git <toonmarvie@gmail.com>
2026-09-09 23:05:38 +0100

    W02 Learning Activity: Handling DOM Events

--- week02/scripts/bom-scripts.js
+++ week02/scripts/bom-scripts.js
@@ -1,14 +1,26 @@
 const inputaValue = document.querySelector("#favchap");
 
-const button = document.querySelector("buutton");
+const button = document.querySelector("button");
 
 const ulList = document.querySelector("ul");
 
-const newList = document.createElement("li");
-newList.textContent = inputaValue.value;
-const deleteButton = document.createElement("button");
-deleteButton.textContent = "❌";
-deleteButton.setAttribute("aria-label", "Close");
-deleteButton.id = "close-button";
-newList.append(deleteButton);
-ulList.append(newList);
+
+
+button.addEventListener("click", function(){
+  if (inputaValue.value.trim() !== "") {
+    const newList = document.createElement("li");
+    newList.textContent = inputaValue.value;
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "❌";
+    deleteButton.setAttribute("aria-label", "Close");
+    deleteButton.id = "close-button";
+    newList.append(deleteButton);
+    ulList.append(newList);
+    inputaValue.focus();
+    inputaValue.value = '';
+    deleteButton.addEventListener("click", function() {
+        newList.remove();
+        inputaValue.focus();
+    })
+}
+});
